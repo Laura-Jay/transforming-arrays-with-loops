d24ca7cda1ea70dc33ecfcac1ab01fa2b2ae0f61
laura J <laura@academy.com>
2022-02-21 22:44:32 +0000

    fixed bug in 1f-pokemon-types.js

--- 1f-pokemon-types.js
+++ 1f-pokemon-types.js
@@ -4,21 +4,10 @@
 //and returns a descriptive sentence about them and their type: "<name> is a <type in lowercase> type Pokémon.".
 
 function pokemonType(pokemonArray){
-const fireType = ["charmander", "growlithe", "ponyta", "arcinine", "rapidash", "charmeleon", "charizard"];
-const waterType = ["squirtle", "warturtle", "blastoise", "staryu", "starmie", "psyduck"];
-const grassType = ["bulbasaur", "ivysaur", "venosaur"];
 const pokedex = []; 
 
 for (let pokemon of pokemonArray){
-    if (fireType.includes(pokemonArray.name)){
-    pokedex.push(pokemonArray.name, " is a fire type Pokemon.");
-    } else if (waterType.includes(pokemonArray.name)){
-        pokedex.push(pokemonArray.name, " is a water type Pokemon.");
-    }       else if (grassType.includes(pokemonArray.name)){
-            pokedex.push(pokemonArray.name, " is a grass type Pokemon.");
-    }           else {
-                pokedex.push("Pokemon type not found.");    
-    } 
+pokedex.push(pokemon.name + " is a " + pokemon.type + " type Pokemon.");
 }
 return pokedex;
 }
@@ -27,3 +16,4 @@ return pokedex;
 
 console.log(pokemonType([{ name: 'Bulbasaur', type: 'Grass' }, { name: 'Charmander', type: 'Fire' }, { name: 'Squirtle', type: 'Water' }]), 
 "[Bulbasaur is a grass type Pokémon., Charmander is a fire type Pokémon., Squirtle is a water type Pokémon.]");
+
